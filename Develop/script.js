ce37a8c8ec2917f077d1e728916c99103e8ce69f
Alyssa Cuson <acuson98@gmail.com>
2021-11-13 20:58:22 -0500

    adding tweaks and comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Password variable that we want to return
+var password = ""
+
+// Generate Password function
+function generatePassword () {
+  console.log("Clicked the button")
+
+  // Prompt the user
+  
+  var passwordLength = prompt("How long do you want your password to be?"); 
+  
+  if (passwordLength < 8) {
+    alert("The password must be at least 8 characters.")
+  } else (passwordLength > 128); {
+    alert("The password must be no more than 128 characters.")
+  }
+
+  if (passwordLength >= 8, passwordLength <=128); {
+    var lower = confirm("Do you want to include lower case letters?");
+    var upper = confirm("Do you want to include upper case letters?");
+    var num = confirm("Do you want to include numbers?");
+    var special = confirm("Do you want to include speacial characters?");
+  }
+
+   // If else statements for character criteria
+   if (lower === true) {
+    var lowerChar = "abcdefghijklmnopqrstuvwxyz"
+    console.log(true)
+  } else (lower === fasle); {
+    console.log(false);
+  }
+    
+  if (upper === true) {
+    var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    console.log(true)
+  } else (upper === false); {
+    console.log(false);
+  }
+  if (num === true) {
+    var numChar = "0123456789"
+    console.log(true)
+  } else (num === false); {
+    console.log(false);
+  }
+
+  if (special === true) {
+    var specialChar = " !'#$%&()*+,-./:;<=>?@[]\^_`{|}~"
+    console.log(true)
+  } else (special === false); {
+    console.log(false);
+  }
+
+  //Randomly generating character criteria
+ 
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * lowerChar.length);
+    password += lowerChar.substring(randomNumber, randomNumber + 1);
+    var randomNumber = Math.floor(Math.random() * upperChar.length);
+    password += upperChar.substring(randomNumber, randomNumber + 1);
+    var randomNumber = Math.floor(Math.random() * numChar.length);
+    password += numChar.substring(randomNumber, randomNumber + 1);
+    var randomNumber = Math.floor(Math.random() * specialChar.length);
+    password += specialChar.substring(randomNumber, randomNumber + 1);
+
+  }
+return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,63 +80,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !'#$%&()*+,-./:;<=>?@[]\^_`{|}~";
-
-// var password = "";
-
-// function getPasswordInfo () {
-  
-// }
-
-function generatePassword () {
-  
-  var passwordLength = prompt("How long do you want your password to be?") 
-  if (passwordLength < 8) {
-    alert("The password must be at least 8 characters.")
-  } else if (passwordLength > 128) {
-    alert("The password must be no more than 128 characters.")
-  } else (passwordLength >= 8, passwordLength <=128); {
-    var lower = confirm("Do you want to include lower case letters?");
-    var upper = confirm("Do you want to include upper case letters?");
-    var num = confirm("Do you want to include numbers?");
-    var special = confirm("Do you want to include speacial characters?");
-  }
-// Need these if/else statements?
-  if (lower === true) {
-    var lowerChar = "abcdefghijklmnopqrstuvwxyz"
-  } else (lower = fasle);
-
-  if (upper === true) {
-    var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  } else (upper = false);
-
-  if (num === true) {
-    var numChar = "0123456789"
-  } else (num = false);
-
-  if (special === true) {
-    var specialChar = " !'#$%&()*+,-./:;<=>?@[]\^_`{|}~"
-  } else (special = false);
- 
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * lowerChar.length);
-    password += lowerChar.substring(randomNumber, randomNumber + 1);
-  }
-
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * upperChar.length);
-    password += upperChar.substring(randomNumber, randomNumber + 1);
-  }
-
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * numChar.length);
-    password += numChar.substring(randomNumber, randomNumber + 1);
-  }
-
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * specialChar.length);
-    password += specialChar.substring(randomNumber, randomNumber + 1);
-  }
-  return password;
-}
